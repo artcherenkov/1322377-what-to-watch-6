bc0d0494b8da60e0a557c28ca98f99988476180e
Artem Cherenkov <art.cherenkov@gmail.com>
2021-03-20 15:05:55 +0700

    Обновлен компонент NotFoundPage

--- src/pages/not-found-page/not-found-page.jsx
+++ src/pages/not-found-page/not-found-page.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import './styles.css';
 
-const NotFoundPage = () => (
+import './styles.css';
+import propTypes from './not-found-page.props';
+
+const NotFoundPage = (props) => (
   <section className="error-page">
-    <h1 className="error-page__header">Страница не найдена</h1>
+    <h1 className="error-page__header">{props.title || `Страница не найдена`}</h1>
     <a className="error-page__link" href="/">Вернуться на главную</a>
   </section>
 );
+
+NotFoundPage.propTypes = propTypes;
 
 export default NotFoundPage;
