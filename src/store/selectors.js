a60bd4b26e7212dae691b7f871265660284711ed
Artem Cherenkov <art.cherenkov@gmail.com>
2021-03-20 15:18:27 +0700

    Удалены лишние селекторы

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,4 +1,2 @@
 export const getMovies = (state) => state.movies;
-export const getFilteredMovies = (state) => state.filteredMovies;
 export const getActiveGenre = (state) => state.activeGenre;
-export const getMovieCardsToShowCount = (state) => state.movieCardsToShowCount;
