ff0ef59ad5195abe95a71cc45b88fb5042fd5faf
Artem Cherenkov <art.cherenkov@gmail.com>
2021-03-20 15:41:29 +0700

    Добавлена возможность отображения статуса авторизации в хедере

--- src/components/user-block/user-block.jsx
+++ src/components/user-block/user-block.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import {useSelector, shallowEqual} from "react-redux";
+import {getAuthStatus} from "../../store/selectors";
+import {AuthorizationStatus} from "../../const";
+import {Link} from "react-router-dom";
 
 function UserBlock() {
+  const authStatus = useSelector(getAuthStatus, shallowEqual);
   return (
     <div className="user-block">
-      <div className="user-block__avatar">
-        <img src="img/avatar.jpg" alt="User avatar" width={63} height={63} />
-      </div>
+
+      {authStatus === AuthorizationStatus.AUTH
+        ? (
+          <div className="user-block__avatar">
+            <img src="img/avatar.jpg" alt="User avatar" width={63} height={63} />
+          </div>
+        )
+        : <Link className="user-block__link" to="/login">Sign in</Link>}
+
     </div>
   );
 }
