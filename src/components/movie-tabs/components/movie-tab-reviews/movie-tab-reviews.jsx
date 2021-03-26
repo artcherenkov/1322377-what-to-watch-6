41a0cf3c2358d36e567377069a94a48d99e2d028
Artem Cherenkov <art.cherenkov@gmail.com>
2021-03-26 23:47:09 +0700

    Исправлена инициализация ф-ции и key компонентов в компоненте MovieTabReviews

--- src/components/movie-tabs/components/movie-tab-reviews/movie-tab-reviews.jsx
+++ src/components/movie-tabs/components/movie-tab-reviews/movie-tab-reviews.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import {nanoid} from "nanoid";
 
 import ReviewsCol from "./components/reviews-col/reviews-col";
 import Review from "./components/review/review";
 
 import propTypes from './movie-tab-reviews.props';
 
-function splitReviews(reviews) {
+const splitReviews = (reviews) => {
   if (!reviews) {
     return null;
   }
@@ -18,7 +17,7 @@ function splitReviews(reviews) {
   const middle = Math.ceil(reviews.length / 2);
 
   return [reviews.slice(0, middle), reviews.slice(middle)];
-}
+};
 
 const MovieTabReviews = (props) => {
   const {reviews, isLoading} = props;
@@ -40,8 +39,8 @@ const MovieTabReviews = (props) => {
   return (
     <>
       <div className="movie-card__reviews movie-card__row">
-        {dividedReviews.map((reviewsPartArr) => <ReviewsCol key={nanoid()}>
-          {reviewsPartArr.map((review) => <Review key={nanoid()} review={review}/>)}
+        {dividedReviews.map((reviewsPartArr, i) => <ReviewsCol key={`review-column-${i}`}>
+          {reviewsPartArr.map((review, j) => <Review key={`review-${j}`} review={review}/>)}
         </ReviewsCol>)}
       </div>
     </>
